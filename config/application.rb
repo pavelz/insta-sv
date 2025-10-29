a378eb604a8144fe28a5f94b825af5546c9afa89
Pavel Zaitsev <pavel@arslogi.ca>
2025-10-28 22:39:39 -0400

    misc

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,11 @@ module InstaSv
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    
 
+    config.generators do |g|
+      g.test_framework :rspec
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
